159352e307dc75cfc987bb309b3f10fb57c2a11c
Paulo Fernando <paulo.fdo@hotmail.com>
2021-06-26 15:11:59 -0300

    Integração Dialog com Page EmployeeOverview

--- PieShop.App/Pages/EmployeeOverview.cs
+++ PieShop.App/Pages/EmployeeOverview.cs
@@ -1,4 +1,6 @@
-﻿using PieShop.App.Services;
+﻿using Microsoft.AspNetCore.Components;
+using PieShop.App.Components;
+using PieShop.App.Services;
 using PieShop.Shared;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +10,9 @@ namespace PieShop.App.Pages
 {
     public partial class EmployeeOverview
     {
-        private readonly IEmployeeDataService _employeeDataService;
-        private readonly IJobCategoryDataService _jobCategoryDataService;
-        private readonly ICountryDataService _countryDataService;
 
-        public EmployeeOverview(IEmployeeDataService employeeDataService, IJobCategoryDataService jobCategoryDataService, ICountryDataService countryDataService)
-        {
-            _employeeDataService = employeeDataService;
-            _jobCategoryDataService = jobCategoryDataService;
-            _countryDataService = countryDataService;
-        }
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
 
         public IEnumerable<Employee> Employees { get; set; }
 
@@ -27,9 +22,20 @@ namespace PieShop.App.Pages
 
         protected override async Task OnInitializedAsync()
         {
-            Employees = (await _employeeDataService.GetAllEmployees()).ToList();
-            Countries = (await _countryDataService.GetAllCountries()).ToList();
-            JobCategories = (await _jobCategoryDataService.GetAllJobCategories()).ToList();
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
         }
     }
 }
