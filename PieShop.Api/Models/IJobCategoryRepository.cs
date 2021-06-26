4d4a8dfca8f4bdfcbfe2ebbc0d43a7c7106fa9ca
Paulo Fernando <paulo.fdo@hotmail.com>
2021-06-26 13:07:45 -0300

    Criação de API.

---
+++ PieShop.Api/Models/IJobCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using PieShop.Shared;
+
+namespace PieShop.Api.Models
+{
+    public interface IJobCategoryRepository
+    {
+        IEnumerable<JobCategory> GetAllJobCategories();
+        JobCategory GetJobCategoryById(int jobCategoryId);
+    }
+}
---
+++ PieShop.Api/Startup.cs
@@ -0,0 +1,62 @@
+using PieShop.Api.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PieShop.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase(databaseName: "PieShop"));
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICountryRepository, CountryRepository>();
+            services.AddScoped<IJobCategoryRepository, JobCategoryRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+			services.AddCors(options =>
+            {
+                options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("Open");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
