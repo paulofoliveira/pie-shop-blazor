90aec47ca5f80f6e3ece793910322f2870278e36
Paulo Fernando <paulo.fdo@hotmail.com>
2021-06-26 22:55:37 -0300

    Adicionado ComponentsLibrary

--- PieShop.App/Pages/EmployeeDetail.cs
+++ PieShop.App/Pages/EmployeeDetail.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using PieShop.App.Services;
+using PieShop.ComponentsLibrary.Map;
 using PieShop.Shared;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PieShop.App.Pages
@@ -11,11 +13,19 @@ namespace PieShop.App.Pages
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
+
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+
+            MapMarkers = new List<Marker>
+            {
+                new Marker{Description = $"{Employee.FirstName} {Employee.LastName}",  ShowPopup = false, X = Employee.Longitude, Y = Employee.Latitude}
+            };
         }
     }
 }
