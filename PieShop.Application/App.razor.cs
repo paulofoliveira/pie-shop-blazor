fdde3fb5a7e7487063709a624fb1a685a5d86444
Paulo Fernando Oliveira <paulo.fdo@hotmail.com>
2021-11-14 19:27:57 -0300

    Lazy Loading (#7)
    * Marker movido para Shared
    
    * ComponentsLibrary carregado em página correta

---
+++ PieShop.Application/App.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PieShop.Application
+{
+    public partial class App
+    {
+        private List<Assembly> lazyLoadedAssemblies = new();
+
+        [Inject]
+        public LazyAssemblyLoader LazyAssemblyLoader { get; set; }
+
+        private async Task OnNavigateAsync(NavigationContext args)
+        {
+            if (args.Path.Contains("employeedetail"))
+            {
+                var assemblies = await LazyAssemblyLoader.LoadAssembliesAsync(new string[] { "PieShop.ComponentsLibrary.dll" });
+                lazyLoadedAssemblies.AddRange(assemblies);
+            }
+        }
+    }
+}
---
+++ PieShop.ComponentsLibrary/Map/Map.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PieShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PieShop.ComponentsLibrary.Map
+{
+    public partial class Map
+    {
+        readonly string elementId = $"map-{Guid.NewGuid():D}";
+
+        [Parameter]
+        public double Zoom { get; set; }
+
+        [Parameter]
+        public List<Marker> Markers { get; set; }
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("deliveryMap.showOrUpdate", elementId, Markers);
+        }
+    }
+}
