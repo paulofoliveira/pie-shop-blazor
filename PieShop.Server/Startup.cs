7a82e06a58cc8ecae6d08e0d001e7c325554b692
Paulo Fernando Oliveira <paulo.fdo@hotmail.com>
2021-11-14 14:07:54 -0300

    PieShop Server (#2)
    * Criado projeto PieShop.Server
    
    * Fix: Title e comentários

---
+++ PieShop.Server/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PieShop.Application.Services;
+using System;
+
+namespace Pieshop.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            var apiUri = new Uri("https://localhost:44340/");
+
+            services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client =>
+            {
+                client.BaseAddress = apiUri;
+            });
+            services.AddHttpClient<ICountryDataService, CountryDataService>(client =>
+            {
+                client.BaseAddress = apiUri;
+            });
+            services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client =>
+            {
+                client.BaseAddress = apiUri;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");            
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
